5f0cbf7faa7549de392e5884ac5e60d88787af7d
yakupkose3123 <yakupkose3123@gmail.com>
2022-04-11 22:19:57 +0300

    created navbar and register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="form-image">
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3">Register</h1>
+        <form id="register">
+          <label htmlFor="first-name" className="form-label">
+            First Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="first-name"
+            placeholder="Enter your first name.."
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
