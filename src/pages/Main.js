2f644cb2f1586fd54ada4b9699197c66a516e916
yakupkose3123 <yakupkose3123@gmail.com>
2022-04-20 18:08:15 +0300

    created details page and serach bar

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,20 @@
 import axios from "axios";
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import MovieCard from "../components/MovieCard";
-
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate} from "react-router-dom"
+ 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 
 const Main = () => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const {currentUser} = useContext(AuthContext);
+  const navigate = useNavigate();
+
   useEffect(() => {
     getMovies(FEATURED_API);
    
@@ -19,10 +25,34 @@ const Main = () => {
     .get(API)
     .then((res) => setMovies(res.data.results))
     .catch((err) => console.log(err))
-  }
+  };
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    if(searchTerm && currentUser){
+      getMovies(SEARCH_API + searchTerm);
+    }else if (!currentUser){
+      navigate("/login")
+    }else{
+      alert("PLEASE ENTER A TEXT");
+    }
+    
+
+  };
 
   return (
     <>
+    <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search a movie..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
     <div className="d-flex justify-content-center flex-wrap">
       {movies.map((movie) => (
         <MovieCard key={movie.id} {...movie} />
